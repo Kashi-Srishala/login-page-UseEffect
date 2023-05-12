f41cb8cdd6f90246a3e7a747bb2c02dd6e5f1923
Kashi-Srishala <kashisrishala@gmail.com>
2023-05-12 12:21:30 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import AuthContext from './store/auth-context';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
@@ -18,13 +19,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout : logoutHandler
+    }}>
+        <MainHeader />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
   );
 }
 
